c91d3f6e08b54b21f47c641cb0ffccb457e70bdf
rauhut <jackierauhut@gmail.com>
2020-07-15 16:34:44 -0400

    handle create recipe form validation

--- front-end/src/components/AddRecipeModal/AddRecipeModal.js
+++ front-end/src/components/AddRecipeModal/AddRecipeModal.js
@@ -10,6 +10,8 @@ import {
   InputGroup,
   Button,
   InputGroupAddon,
+  FormFeedback,
+  Alert,
 } from "reactstrap";
 import "./AddRecipeModal.css";
 
@@ -19,56 +21,114 @@ class AddRecipeModal extends React.Component {
     this.state = {
       isCreateRecipeOpen: false,
       recipeName: "",
+      isNameInvalid: false,
       recipeCookTime: "",
+      isCookTimeInvalid: false,
       recipeIngredients: [""],
       recipeSteps: [""],
       recipeDescription: "",
+      isDescriptionInvalid: false,
       recipePicture: "",
+      isPictureInvalid: false,
+      errMsg: "Unable to create recipe",
+      invalidRecipe: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onSumbitRecipe = () => {
-    const token = window.sessionStorage.getItem("token");
-    console.log(this.state);
-    fetch("http://localhost:3000/recipe", {
-      method: "post",
-      headers: { "Content-Type": "application/json", Authorization: token },
-      body: JSON.stringify({
-        name: this.state.recipeName,
-        cookTime: this.state.recipeCookTime,
-        ingredients: this.state.recipeIngredients,
-        steps: this.state.recipeSteps,
-        description: this.state.recipeDescription,
-        picture: this.state.recipePicture,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.id) {
-          this.props.toggle();
-          this.props.navToRecipe(data.id);
-        } else {
-          throw new Error("Something went wrong when trying to add recipe");
-        }
+    const recipeIngredients = this.state.recipeIngredients.filter(
+      (ingredient) => ingredient.trim() != ""
+    );
+    const recipeSteps = this.state.recipeSteps.filter(
+      (step) => step.trim() != ""
+    );
+
+    // Verify that ingredient and direction list are not empty
+    if (recipeIngredients.length === 0) {
+      this.setState({ errorMsg: "Please enter at least one ingredient" });
+      this.setState({ invalidRecipe: true });
+    } else if (recipeSteps.length === 0) {
+      this.setState({ errorMsg: "Please enter at least one direction" });
+      this.setState({ invalidRecipe: true });
+    } else {
+      this.setState({ invalidRecipe: false });
+
+      const token = window.sessionStorage.getItem("token");
+
+      // verify valid form input
+      if (this.state.recipeName === "") {
+        this.setState({ isNameInvalid: true });
+      }
+      if (this.state.recipeDescription === "") {
+        this.setState({ isDescriptionInvalid: true });
+      }
+      if (this.state.recipeCookTime === "") {
+        this.setState({ isCookTimeInvalid: true });
+      }
+      if (!/\.(jpeg|jpg|gif|png)$/.test(this.state.recipePicture)) {
+        this.setState({ isPictureInvalid: true });
+      }
+
+      fetch("http://localhost:3000/recipe", {
+        method: "post",
+        headers: { "Content-Type": "application/json", Authorization: token },
+        body: JSON.stringify({
+          name: this.state.recipeName,
+          cookTime: this.state.recipeCookTime,
+          ingredients: recipeIngredients,
+          steps: recipeSteps,
+          description: this.state.recipeDescription,
+          picture: this.state.recipePicture,
+        }),
       })
-      .catch(console.log);
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.id) {
+            this.props.toggle();
+            this.props.navToRecipe(data.id);
+          } else {
+            throw new Error("Something went wrong when trying to add recipe");
+          }
+        })
+        .catch(console.log);
+    }
   };
 
   onNameChange = (event) => {
     this.setState({ recipeName: event.target.value });
+    let isNameInvalid = false;
+    if (event.target.value.length === 0) {
+      isNameInvalid = true;
+    }
+    this.setState({ isNameInvalid });
   };
 
   onCookTimeChange = (event) => {
     this.setState({ recipeCookTime: event.target.value });
+    let isCookTimeInvalid = false;
+    if (event.target.value.length === 0) {
+      isCookTimeInvalid = true;
+    }
+    this.setState({ isCookTimeInvalid });
   };
 
   onDescriptionChange = (event) => {
     this.setState({ recipeDescription: event.target.value });
+    let isDescriptionInvalid = false;
+    if (event.target.value.length === 0) {
+      isDescriptionInvalid = true;
+    }
+    this.setState({ isDescriptionInvalid });
   };
 
   onPictureChange = (event) => {
     this.setState({ recipePicture: event.target.value });
+    let isPictureInvalid = false;
+    if (!/\.(jpeg|jpg|gif|png)$/.test(event.target.value)) {
+      isPictureInvalid = true;
+    }
+    this.setState({ isPictureInvalid });
   };
 
   createIngredientsList() {
@@ -86,7 +146,7 @@ class AddRecipeModal extends React.Component {
                 color="danger"
                 onClick={this.removeIngredientsClick.bind(this, i)}
               >
-                -
+                Remove
               </Button>
             </InputGroupAddon>
           ) : null}
@@ -128,7 +188,7 @@ class AddRecipeModal extends React.Component {
                 color="danger"
                 onClick={this.removeStepsClick.bind(this, i)}
               >
-                -
+                Remove
               </Button>
             </InputGroupAddon>
           ) : null}
@@ -175,7 +235,9 @@ class AddRecipeModal extends React.Component {
                 id="recipeName"
                 placeholder="Add Recipe Name"
                 onChange={this.onNameChange}
+                invalid={this.state.isNameInvalid}
               />
+              <FormFeedback>Recipe name must not be empty</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label>Description</Label>
@@ -185,7 +247,9 @@ class AddRecipeModal extends React.Component {
                 id="description"
                 placeholder="Add a brief description of the recipe"
                 onChange={this.onDescriptionChange}
+                invalid={this.state.isDescriptionInvalid}
               />
+              <FormFeedback>Recipe description must not be empty</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label>Total Cook Time</Label>
@@ -194,10 +258,11 @@ class AddRecipeModal extends React.Component {
                 name="cookTime"
                 id="cookTime"
                 onChange={this.onCookTimeChange}
+                invalid={this.state.isCookTimeInvalid}
               />
+              <FormFeedback>Recipe cook time must not be empty</FormFeedback>
             </FormGroup>
-
-            <FormGroup>
+            <FormGroup invalid>
               <Label>Ingredients</Label>
               {this.createIngredientsList()}
               <input
@@ -206,10 +271,11 @@ class AddRecipeModal extends React.Component {
                 value="Add row"
                 onClick={this.addIngredientsClick.bind(this)}
               />
+              <FormFeedback>Recipe cook time must not be empty</FormFeedback>
             </FormGroup>
 
-            <FormGroup>
-              <Label>Steps</Label>
+            <FormGroup invalid>
+              <Label>Directions</Label>
               {this.createStepsList()}
               <input
                 className="button-secondary"
@@ -227,9 +293,16 @@ class AddRecipeModal extends React.Component {
                 id="photo"
                 placeholder="Add a URL for a photo to use for this recipe"
                 onChange={this.onPictureChange}
+                invalid={this.state.isPictureInvalid}
               />
+              <FormFeedback>Photo must be a valid image URL</FormFeedback>
             </FormGroup>
           </Form>
+          <div>
+            {this.state.invalidRecipe && (
+              <Alert color="danger">{this.state.errorMsg}</Alert>
+            )}
+          </div>
         </ModalBody>
         <ModalFooter>
           <button className="button-secondary" onClick={this.onSumbitRecipe}>
