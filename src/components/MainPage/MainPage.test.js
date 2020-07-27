9ffe5dd43249d62ba4432528d91f014aa4672091
rauhut <jackierauhut@gmail.com>
2020-07-27 13:46:01 -0400

    remove unused imports

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import MainPage from "./MainPage";
-import { MemoryRouter } from "react-router-dom";
 
 describe("RecipeDisplay", () => {
   let wrapper;
